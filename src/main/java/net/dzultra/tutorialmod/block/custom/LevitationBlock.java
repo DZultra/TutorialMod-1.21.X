00a564eaabe24a88134c101fa1baa8ad22248f0e
dzultra <DanielZ123@web.de>
2025-01-25 14:47:09 +0100

    Changed: - LevitationBlock to now only give Levitation if the block below is emitting redstone power

--- src/main/java/net/dzultra/tutorialmod/block/custom/LevitationBlock.java
+++ src/main/java/net/dzultra/tutorialmod/block/custom/LevitationBlock.java
@@ -8,19 +8,19 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class LevitationBlock extends Block {
     public LevitationBlock(Settings settings) {
         super(settings);
     }
-    boolean active = false;
 
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (!(world instanceof ServerWorld serverWorld)) return;
         if (entity instanceof LivingEntity livingEntity) {
-            if (world.isReceivingRedstonePower(pos)) {
+            if (world.getEmittedRedstonePower(pos.down(), Direction.DOWN) > 0) {
                 livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 100, 2)); // Effect Level is calculated by "Amplifier + 1"
             }
         }
