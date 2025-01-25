1e55ac0bf8dc845300431d3befed4357867d0537
dzultra <DanielZ123@web.de>
2025-01-25 14:06:14 +0100

    Added: -OnStepSpawnerBlock

--- src/main/java/net/dzultra/tutorialmod/block/custom/OnStepSpawnerBlock.java
+++ src/main/java/net/dzultra/tutorialmod/block/custom/OnStepSpawnerBlock.java
@@ -1,19 +1,34 @@
 package net.dzultra.tutorialmod.block.custom;
 
+import net.dzultra.tutorialmod.TutorialMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 public class OnStepSpawnerBlock extends Block {
+    public static final BooleanProperty PLAYER_ON_BLOCK = BooleanProperty.of("player_on_block");
+
     public OnStepSpawnerBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.getDefaultState()
+                .with(PLAYER_ON_BLOCK, false)
+        );
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(PLAYER_ON_BLOCK);
     }
 
     @Override
@@ -21,19 +36,39 @@ public class OnStepSpawnerBlock extends Block {
         if (!(world instanceof ServerWorld serverWorld)) return;
         if (!(entity instanceof PlayerEntity)) return;
 
-        net.minecraft.util.math.random.Random random = serverWorld.getRandom();
+        serverWorld.setBlockState(pos, state.with(PLAYER_ON_BLOCK, true));
+        serverWorld.scheduleBlockTick(pos, this, 10);
+    }
+
+    @Override
+    protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        boolean playerStillOnBlock = !world.getEntitiesByClass(PlayerEntity.class, new Box(pos.up()), entity -> true).isEmpty();
+        if (playerStillOnBlock) return;
+        world.setBlockState(pos, state.with(PLAYER_ON_BLOCK, false));
+    }
+
+    @Override
+    protected void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        super.onStateReplaced(state, world, pos, newState, moved);
+        if (!(world instanceof ServerWorld serverWorld)) return;
+
+        if (!state.isOf(newState.getBlock())) return;
+
+        if (!state.get(PLAYER_ON_BLOCK) && newState.get(PLAYER_ON_BLOCK)) {
+            spawnCreeper(serverWorld, pos);
+        }
+    }
+
+    private void spawnCreeper(ServerWorld world, BlockPos pos) {
+        net.minecraft.util.math.random.Random random = world.getRandom();
 
         int xOffset = random.nextInt(11) - 5; // Bereich: -5 bis 5
         int zOffset = random.nextInt(11) - 5;
         BlockPos spawnPos = pos.add(xOffset, 0, zOffset);
 
-        BlockPos groundPos = serverWorld.getTopPosition(net.minecraft.world.Heightmap.Type.WORLD_SURFACE, spawnPos);
-        if (!serverWorld.getBlockState(groundPos).isAir()) {
-            spawnPos = groundPos.up();
-        }
-
-        ZombieEntity zombie = new ZombieEntity(serverWorld);
-        zombie.refreshPositionAndAngles(Vec3d.ofCenter(spawnPos), random.nextFloat() * 360F, 0);
-        serverWorld.spawnEntity(zombie);
+        CreeperEntity creeper = new CreeperEntity(EntityType.CREEPER, world);
+        creeper.refreshPositionAndAngles(Vec3d.ofCenter(spawnPos), random.nextFloat() * 360F, 0);
+        world.spawnEntity(creeper);
     }
 }
+
