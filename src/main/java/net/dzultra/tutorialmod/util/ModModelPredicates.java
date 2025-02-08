cbd8156acd4363ddc56bdd8ffcec4a947f107d6a
dzultra <DanielZ123@web.de>
2025-02-08 15:56:36 +0100

    Added: - Model Predicate "used" to Chisel

---
+++ src/main/java/net/dzultra/tutorialmod/util/ModModelPredicates.java
@@ -0,0 +1,15 @@
+package net.dzultra.tutorialmod.util;
+
+import net.dzultra.tutorialmod.TutorialMod;
+import net.dzultra.tutorialmod.component.ModDataComponentTypes;
+import net.dzultra.tutorialmod.item.ModItems;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
+
+public class ModModelPredicates {
+    public static void registerModelPredicates() {
+        ModelPredicateProviderRegistry.register(ModItems.CHISEL, Identifier.of(TutorialMod.MOD_ID, "used"),
+                (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.COORDINATES) != null ? 1f : 0f);
+
+    }
+}
